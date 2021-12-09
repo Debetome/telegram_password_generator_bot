2f5d97c25f2a350d2207215438fbef4da91954ca
Debetome <magregory@tutanota.com>
2021-12-08 23:01:19 -0500

    Writing database handler

--- PGBot/handlers/dbHandler.py
+++ PGBot/handlers/dbHandler.py
@@ -1,8 +1,10 @@
 import sqlite3
 import os
 
-from typing import List, Union 
-from PGBot.models import *
+from typing import List, Union, Any
+
+from PGBot.models import PasswordRegister
+from PGBot.models import User
 from PGBot.tables import *
 
 class DatabaseHandler:
@@ -19,20 +21,69 @@ class DatabaseHandler:
         for table in TABLES.values():
             self._execute(table)
 
-    def get_passwords(self, chat_id: int) -> List[tuple]:
+    def get_user(self, chat_id: int) -> User:
+        self._execute(f"SELECT * FROM Users WHERE Chat_id=\"{chat_id}\"", fetchone=True)
+        user: User = User(
+            firstname=self._response[1],
+            lastname=self._response[2],
+            chat_id=self._response[3]
+        )
+
+        return user
+
+    def get_users(self) -> List[User]:
+        self._execute("SELECT * FROM Users")
+
+        users: List[User] = []
+        for user in self._response:
+            users.append(User(
+                firstname=user[1],
+                lastname=user[2],
+                chat_id=int(user[3])
+            ))
+
+        return users
+
+    def get_passwords(self, chat_id: int) -> List[PasswordRegister]:
         self._execute(f"SELECT * FROM Passwords WHERE chat_id='{chat_id}'")
-        return self._response
 
-    def get_password(self, chat_id: int, title: str) -> tuple:
-        self._execute(f"SELECT * FROM Passwords WHERE chat_id='{chat_id}' title='{title}'", fetchone=True)
+        passwords: List[PasswordRegister] = []
+        for password in self._response:
+            passwords.append(PasswordRegister(
+                title=password[1],
+                password=password[2],
+                chat_id=int(password[3])
+            ))
 
-    def insert_password(self, password: Password) -> None:
+        return passwords
+
+    def insert_user(self, user: User) -> None:
+        firstname = user.firstname
+        lastname = user.lastname
+        chat_id = user.chat_id
+
+        self._execute(f"INSERT INTO Users(Firstname, Lastname, Chat_id) VALUES(\"{firstname}\", \"{lastname}\", \"{chat_id}\")")
+        self._response = f"New user {firstname} {lastname} inserted!"
+
+    def insert_password(self, password: PasswordRegister) -> None:
         title = password.title
         passwd = password.password
         chat_id = password.chat_id
 
         self._execute(f"INSERT INTO Passwords(Title, Password, Chat_id) VALUES(\"{title}\", \"{passwd}\", \"{chat_id}\")")
         self._response = f"New password inserted {password}"
+
+    def delete_user(self, user: User) -> None:
+        firstname = user.firstname
+        lastname = user.lastname
+        chat_id = user.chat_id
+        self._execute(f"DELETE INTO Users WHERE Chat_id=\"{chat_id}\"")
+        self._response = f"User {firstname} {lastname} deleted!"
+
+    def delete_password(self, password: PasswordRegister) -> None:
+        title = password.title
+        self._execute(f"DELETE INTO Passwords WHERE Title=\"{title}\"")
+        self._response = "Password titled '{title}' deleted!"
 
     @property
     def database(self) -> str:
@@ -41,6 +92,14 @@ class DatabaseHandler:
     @property
     def response(self) -> Union[Union[str, tuple], None]:
         return self._response
+
+    @property
+    def connection(self) -> Any:
+        return self._conn
+
+    @property
+    def cursor(self) -> Any:
+        return self._cursor
 
     def _execute(self, command: str, fetchone=False) -> Union[List[str], str]:
         self._connect()
